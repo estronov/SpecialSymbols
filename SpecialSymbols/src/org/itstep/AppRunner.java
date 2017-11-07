fc4501cf9c4647b88cba48890f1688fffc107295
estronov <estronov@LAPTOP-K5V9QFC8>
2017-11-07 22:09:34 +0200

    1

--- SpecialSymbols/src/org/itstep/AppRunner.java
+++ SpecialSymbols/src/org/itstep/AppRunner.java
@@ -2,6 +2,11 @@ package org.itstep;
 
 public class AppRunner {
 	public static void main (String[] args) {
+		String x = "/";
+		String y = "\"";
+		System.out.println(y+x+y+"Alex"+y+x+y);
 	}
-}
+	}
+
+	
 
